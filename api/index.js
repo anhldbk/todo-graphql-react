8344da3b7c0f0d2907427446c2eb8d3defb92551
Anh Le <anhldbk@gmail.com>
2016-12-15 15:01:55 +0700

    Refactored code for API server

--- api/index.js
+++ api/index.js
@@ -1,72 +1,67 @@
-import { createServer } from 'http';
-import { SubscriptionServer } from 'subscriptions-transport-ws';
-import { subscriptionManager } from './subscriptions';
 import hapi from 'hapi';
-const { apolloHapi, graphiqlHapi } = require('apollo-server');
+const {apolloHapi, graphiqlHapi} = require('apollo-server');
 import {GraphQLSchema} from 'graphql';
 import schema from './schema'
+import wsServer from './ws-server';
+const Good = require('good');
 const API_PORT = process.env.API_PORT | 8080
 const WS_PORT = process.env.WS_PORT || 8081;
+
 const server = new hapi.Server();
+server.connection({host: 'localhost', port: API_PORT});
 
-server.connection({
-  host: 'localhost',
-  port: API_PORT
+server.register({
+    register: apolloHapi,
+    options: {
+        path: '/graphql',
+        apolloOptions: () => ({pretty: true, schema})
+    }
 });
 
 server.register({
-  register: apolloHapi,
-  options: {
-    path: '/graphql',
-    apolloOptions: () => ({
-      pretty: true,
-      schema
-    }),
-  },
+    register: graphiqlHapi,
+    options: {
+        path: '/graphiql',
+        graphiqlOptions: {
+            endpointURL: '/graphql'
+        }
+    }
 });
 
 server.register({
-  register: graphiqlHapi,
-  options: {
-    path: '/graphiql',
-    graphiqlOptions: {
-      endpointURL: '/graphql',
-    },
-  },
+    register: Good,
+    options: {
+        reporters: {
+            console: [
+                {
+                    module: 'good-squeeze',
+                    name: 'Squeeze',
+                    args: [
+                        {
+                            response: '*',
+                            log: '*'
+                        }
+                    ]
+                }, {
+                    module: 'good-console'
+                },
+                'stdout'
+            ]
+        }
+    }
+}, (err) => {
+
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start((err) => {
+        if (err) {
+            throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+
+    });
 });
 
-server.start((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Server running at: ${server.info.uri}`);
-});
-
-
-// WebSocket server for subscriptions
-const websocketServer = createServer((request, response) => {
-  response.writeHead(404);
-  response.end();
-});
-
-websocketServer.listen(WS_PORT, () => console.log( // eslint-disable-line no-console
-  `Websocket Server is now running on http://localhost:${WS_PORT}`
-));
-
-// eslint-disable-next-line
-new SubscriptionServer(
-  {
-    subscriptionManager,
-
-    // the obSubscribe function is called for every new subscription
-    // and we use it to set the GraphQL context for this subscription
-    onSubscribe: (msg, params) => {
-      return Object.assign({}, params, {
-        context: {
-          key: 'value'
-        },
-      });
-    },
-  },
-  websocketServer
-);
+wsServer.start(WS_PORT)
---
+++ api/ws-server.js
@@ -0,0 +1,30 @@
+'use strict'
+import {subscriptionManager} from './subscriptions';
+import {createServer} from 'http';
+import {SubscriptionServer} from 'subscriptions-transport-ws';
+
+export default {
+    // Start WebSocket server for GraphQL subscriptions
+    start: function (port) {
+        const websocketServer = createServer((request, response) => {
+            response.writeHead(404);
+            response.end();
+        });
+
+        websocketServer.listen(port, () => console.log(`Websocket Server is now running on http://localhost:${port}`));
+
+        new SubscriptionServer({
+            subscriptionManager,
+
+            // the obSubscribe function is called for every new subscription
+            // and we use it to set the GraphQL context for this subscription
+            onSubscribe: (msg, params) => {
+                return Object.assign({}, params, {
+                    context: {
+                        key: 'value'
+                    }
+                });
+            }
+        }, websocketServer);
+    }
+}
